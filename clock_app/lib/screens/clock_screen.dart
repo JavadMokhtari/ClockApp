088ec44046d0f77c3f403a5c4f3f7b4b15902077
javadmokhtari <ijavad.mokhtari@gmail.com>
2023-05-12 13:19:38 +0430

    Display second-hand of clock

--- clock_app/lib/screens/clock_screen.dart
+++ clock_app/lib/screens/clock_screen.dart
@@ -8,13 +8,23 @@ class ClockScreen extends StatefulWidget {
   State<ClockScreen> createState() => _ClockScreenState();
 }
 
-class _ClockScreenState extends State<ClockScreen> {
+class _ClockScreenState extends State<ClockScreen>
+    with TickerProviderStateMixin {
   late String _timeString;
+  late AnimationController controller;
 
   @override
   void initState() {
     _timeString = _formatDateTime(DateTime.now());
     Timer.periodic(const Duration(seconds: 1), (Timer t) => _getTime());
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 60),
+      value: DateTime.now().second.toDouble() / 60.0,
+    )..addListener(() {
+        setState(() {});
+      });
+    controller.repeat();
     super.initState();
   }
 
@@ -26,8 +36,14 @@ class _ClockScreenState extends State<ClockScreen> {
     });
   }
 
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   String _formatDateTime(DateTime dateTime) {
-    return '${dateTime.hour}:${dateTime.minute}:${dateTime.second}';
+    return '${dateTime.hour}:${dateTime.minute}';
   }
 
   @override
@@ -35,12 +51,27 @@ class _ClockScreenState extends State<ClockScreen> {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(
-            _timeString,
-            style: const TextStyle(
-              fontSize: 32,
+          child: Stack(children: [
+            Center(
+              child: Text(
+                _timeString,
+                style: const TextStyle(
+                  fontSize: 32,
+                  letterSpacing: 3,
+                ),
+              ),
             ),
-          ),
+            Center(
+              child: SizedBox(
+                width: 250,
+                height: 250,
+                child: CircularProgressIndicator(
+                  value: controller.value,
+                  strokeWidth: 2.0,
+                ),
+              ),
+            )
+          ]),
         ),
       ),
     );
