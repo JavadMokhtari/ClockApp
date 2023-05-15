a0393b5c5979832579b8f1e5295d05d64e781a67
javadmokhtari <ijavad.mokhtari@gmail.com>
2023-05-15 13:00:09 +0330

    Updated main.dart

--- clock_app/lib/main.dart
+++ clock_app/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:clock_app/screens/clock_screen.dart';
 import 'package:clock_app/screens/alarm_screen.dart';
+import 'package:clock_app/widgets/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+
+import 'constants/colors.dart';
 
 void main(List<String> args) {
   runApp(const ClockApp());
@@ -17,7 +20,7 @@ class _ClockAppState extends State<ClockApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: AlarmScreen(),
+      home: ClockScreen(),
     );
   }
 }
