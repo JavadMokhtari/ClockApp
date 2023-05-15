8b9c45f9ae83c0ddd431671372a82606ef68ef48
javadmokhtari <ijavad.mokhtari@gmail.com>
2023-05-16 01:16:45 +0330

    Edited one or more colors

--- clock_app/lib/constants/colors.dart
+++ clock_app/lib/constants/colors.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 
 class CustomColors {
   static const background = Color.fromARGB(255, 25, 25, 25);
+  static const foreground = Color.fromARGB(200, 255, 255, 255);
   static const secondaryColor = Color.fromARGB(255, 120, 70, 190);
-  // static const secondaryColor = Colors.deepPurple;
 
   static const navigationBarBackground = Color.fromARGB(255, 18, 18, 18);
   static const alarmCardColor = Color.fromARGB(150, 35, 35, 40);
-  static const screensFontColor = Color.fromARGB(200, 255, 255, 255);
 }
 
 class GradientColors {
