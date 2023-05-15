2d81030d58440216948cd4588bdf74049e4c1948
javadmokhtari <ijavad.mokhtari@gmail.com>
2023-05-15 19:49:13 +0330

    move bottom navigation bar to main page

--- clock_app/lib/main.dart
+++ clock_app/lib/main.dart
@@ -1,26 +1,92 @@
 import 'package:clock_app/screens/clock_screen.dart';
 import 'package:clock_app/screens/alarm_screen.dart';
-import 'package:clock_app/widgets/bottom_navigation_bar.dart';
+import 'package:clock_app/screens/stopwatch_screen.dart';
+import 'package:clock_app/screens/timer_screen.dart';
 import 'package:flutter/material.dart';
-
 import 'constants/colors.dart';
 
 void main(List<String> args) {
-  runApp(const ClockApp());
+  runApp(const ClockHomePage());
 }
 
-class ClockApp extends StatefulWidget {
-  const ClockApp({super.key});
+class ClockHomePage extends StatefulWidget {
+  const ClockHomePage({super.key});
 
   @override
-  State<ClockApp> createState() => _ClockAppState();
+  State<ClockHomePage> createState() => _ClockHomePageState();
 }
 
-class _ClockAppState extends State<ClockApp> {
+class _ClockHomePageState extends State<ClockHomePage> {
+  final List<Widget> _screens = [
+    const AlarmScreen(),
+    const ClockScreen(),
+    const StopwatchScreen(),
+    const TimerScreen(),
+  ];
+  int _screenIndex = 1;
+
+  void _onTapBarItems(int index) {
+    setState(() {
+      _screenIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ClockScreen(),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: CustomColors.background,
+        // floatingActionButton: _screenIndex == 0,
+        body: _screens[_screenIndex],
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: CustomColors.navigationBarBackground,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(40)),
+              child: BottomNavigationBar(
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.alarm),
+                    label: "Alarm",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.av_timer_outlined),
+                    label: "Clock",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.timer_10),
+                    label: "StopWatch",
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.timelapse),
+                    label: "Timer",
+                  ),
+                ],
+                currentIndex: _screenIndex,
+                onTap: _onTapBarItems,
+                backgroundColor: CustomColors.navigationBarBackground,
+                type: BottomNavigationBarType.shifting,
+                selectedItemColor: CustomColors.secondaryColor,
+                unselectedItemColor: Colors.grey,
+                selectedIconTheme: const IconThemeData(
+                  color: CustomColors.secondaryColor,
+                  size: 30,
+                ),
+                unselectedIconTheme:
+                    const IconThemeData(color: Colors.grey, size: 25),
+                selectedLabelStyle:
+                    const TextStyle(fontFamily: "ubuntu", fontSize: 13),
+                unselectedLabelStyle:
+                    const TextStyle(fontFamily: "ubuntu", fontSize: 12),
+                showUnselectedLabels: true,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
