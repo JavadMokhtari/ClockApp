ca4f8353b40306a6b19bd0bfad99ba860743d27b
javadmokhtari <ijavad.mokhtari@gmail.com>
2023-05-18 20:20:49 +0330

    Wrote func to convert Duration and String

--- clock_app/lib/utils/utils.dart
+++ clock_app/lib/utils/utils.dart
@@ -1,67 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:clock_app/constants/constants.dart';
-
-extension ClockExtensions on int {
-  String toTimeString() {
-    if (this < 100) {
-      return toString().padLeft(2, '0');
-    } else {
-      return toString().substring(toString().length - 2);
-    }
+extension DurationToString on Duration {
+  String toTimerString() {
+    String m = inMinutes.toString().padLeft(2, '0');
+    String s = inSeconds.toString().padLeft(2, '0');
+    String ms = inMilliseconds.toString().padLeft(5, '0').substring(2);
+    return "$m:$s:$ms";
   }
 }
 
-// Widget customButton({
-//   required void Function()? onPressed,
-//   required Widget? child,
-// }) {
-//   return
-// }
+extension StringToDuration on String {
+  int toInt() {
+    return int.parse(this);
+  }
 
-class CustomButton extends ElevatedButton {
-  // void Function()? onPressed;
-  // void Function()? onLongPress;
-  // void Function(bool)? onHover;
-  // void Function(bool)? onFocusChange;
-  // ButtonStyle? style;
-  // FocusNode? focusNode;
-  // bool autofocus = false;
-  // Clip clipBehavior = Clip.none;
-  // MaterialStatesController? statesController;
-  // Widget? child;
-
-  const CustomButton({
-    super.key,
-    required super.onPressed,
-    super.onLongPress,
-    super.onHover,
-    super.onFocusChange,
-    super.style,
-    super.focusNode,
-    super.autofocus = false,
-    super.clipBehavior = Clip.none,
-    super.statesController,
-    required super.child,
-  });
-
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: const BorderRadius.all(Radius.circular(40)),
-      child: ElevatedButton(
-        style: const ButtonStyle(
-          backgroundColor:
-              MaterialStatePropertyAll(CustomColors.secondaryColor),
-          textStyle: MaterialStatePropertyAll(
-            TextStyle(
-                fontSize: 20,
-                color: CustomColors.foreground,
-                fontFamily: "ubuntu"),
-          ),
-          fixedSize: MaterialStatePropertyAll(Size(90, 40)),
-        ),
-        onPressed: onPressed,
-        child: child,
-      ),
-    );
+  Duration toTimerDuration() {
+    return Duration(
+        minutes: substring(0, 2).toInt(),
+        seconds: substring(3, 5).toInt(),
+        milliseconds: substring(6, 9).toInt());
   }
 }
